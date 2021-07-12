dbdb7f1697a1bf94cc4aeb06f4d892c755ce0275
limingfei <675669728@qq.com>
2021-07-12 23:08:07 +0800

    修改过bug

--- android/src/main/java/com/soundcloud/android/crop/Crop.java
+++ android/src/main/java/com/soundcloud/android/crop/Crop.java
@@ -1,14 +1,11 @@
 package com.soundcloud.android.crop;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Build;
 import android.provider.MediaStore;
 
-import androidx.fragment.app.Fragment;
 
 
 /**
